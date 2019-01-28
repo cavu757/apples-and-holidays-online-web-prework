1b5c3989ae6ad240e0b9e585a18e6cfe5ec93449
Jose Pehovaz-Diez <jose.pehovazdiez@gmail.com>
2019-01-28 05:53:24 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -60,7 +60,7 @@ def all_holidays_with_bbq(holiday_hash)
   holiday_hash.each do |season, data|
     data.each do |holiday, array|
       array.collect {|item| item == "BBQ"}
-      return array     
+      array     
     end
   end
           
