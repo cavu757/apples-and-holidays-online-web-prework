d7e04daf4a8369beb86182f0a490da3f19f9de50
Jose Pehovaz-Diez <jose.pehovazdiez@gmail.com>
2019-01-28 05:34:20 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -52,6 +52,7 @@ def all_supplies_in_holidays(holiday_hash)
       holiday_s = holiday.to_s
       holiday_s = holiday_s.split("_")
       holiday_s = holiday_s.capitalize
+      holiday_s = holiday
       puts "#{season.capitalize}:"
       puts "  #{holiday_s}: #{array.join(", ")}"
     end
